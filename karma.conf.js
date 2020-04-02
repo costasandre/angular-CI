990b446e3abe2a96613f57f1a106ffc4b4a59142
andre <andre.costa@tpv-tech.com>
2020-04-02 17:48:05 -0400

    test CI

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     customLaunchers: {
-      ChromeHeadlessCI: {
+       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
